64e9a32fd9014f188d9c522ebb802ce0989298f4
sriutaminingsih <49768122+tamiutami@users.noreply.github.com>
2020-11-05 03:38:09 +0700

    Add files via upload

---
+++ perbandinganDuaBuahNilai/src/perbandinganduabuahnilai/PerbandinganDuaBuahNilai.java
@@ -0,0 +1,66 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package perbandinganduabuahnilai;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Sri Utami Ningsih
+ * KELAS : PBO 2
+ * NIM : 10116378
+ * Deskripsi program: program ini berisi program untuk menampilkan hasil
+ * perbandingan dua buah nilai
+ 
+ */
+public class PerbandinganDuaBuahNilai {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner masuk = new Scanner(System.in);
+
+        int nNilai1, nNilai2, Hasil, i;
+        String jawaban, peritah;
+
+        boolean ulang = true;
+
+        while (ulang == true) {
+            System.out.println("Masukan nilai pertama : ");
+            Scanner scanner = new Scanner(System.in);
+            nNilai1 = scanner.nextInt();
+
+            System.out.println("Masukan nilai kedua : ");
+            nNilai2 = scanner.nextInt();
+
+            jawaban = (nNilai1 > nNilai2 ? "besar" : "kecil");
+            System.out.println("Hasil : " + nNilai1 + " Lebih " + jawaban + " dari " + nNilai2);
+            
+            System.out.print("Ulangi Aktifitas ? (ya/tidak) : ");
+            String perintah = scanner.next();
+            
+            if (perintah.equals("tidak")){
+                ulang = false;
+                System.out.println("Keluar");
+            } else {
+                System.out.print("Masukan Nilai Pertama : ");
+                nNilai1 = scanner.nextInt();
+                
+                 System.out.print("Masukan Nilai Kedua : ");
+                nNilai2 = scanner.nextInt();
+            
+                jawaban = (nNilai1 > nNilai2 ? "besar" : "kecil");
+                
+                System.out.println("Hasil : " + nNilai1 + " Lebih " + jawaban + " dari " + nNilai2);
+                System.out.println("Ulang aktifitas ? (ya/tidak) : ");
+                perintah = scanner.next();
+            }
+        }
+    }
+    
+ }
